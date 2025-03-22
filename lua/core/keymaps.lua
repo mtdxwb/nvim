8abfb25c8c59a1dbebfed2b616f34730579bdd19
mtdxwb <zdx1314520666@163.com>
2025-03-22 10:32:30 +0800

    更新keymap添加取消高亮快捷键

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,15 +7,16 @@ local mode_i = { "i" }
 
 local nmappings = {
   -- 功能性设置
-  { from = ";",         to = ":" },
+  { from = ";",           to = ":" },
+  { from = "<leader>nh",  to = ":nohl<CR>" }, -- 取消高亮
 
   -- 移动
-  { from = "J",         to = "5j",                    mode = mode_nv },
-  { from = "K",         to = "5k",                    mode = mode_nv },
+  { from = "J",           to = "5j",                    mode = mode_nv },
+  { from = "K",           to = "5k",                    mode = mode_nv },
 
   -- 移动单行或多行
-  { from = "mj",        to = ":m '>+1<CR>gv=gv",      mode = mode_v  },
-  { from = "mk",        to = ":m '<-2<CR>gv=gv",      mode = mode_v  },
+  { from = "mj",          to = ":m '>+1<CR>gv=gv",      mode = mode_v  },
+  { from = "mk",          to = ":m '<-2<CR>gv=gv",      mode = mode_v  },
 }
 
 for _, mapping in ipairs(nmappings) do
