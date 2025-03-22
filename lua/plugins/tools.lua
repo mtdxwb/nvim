307269c44a34ef35ce63fcae074fc881b60aa51e
mtdxwb <zdx1314520666@163.com>
2025-03-22 09:58:27 +0800

    添加treesitter.nvim 设置notify ==> background_colour='#000000'

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -54,7 +54,44 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    }
-  }
+      {
+        "rcarriga/nvim-notify",
+        config = function()
+          require("notify").setup({
+            background_colour = "#000000",  -- 设置透明背景
+          })
+        end
+      },
+
+      -- treesitter
+      "nvim-treesitter/nvim-treesitter",
+    },
+
+  },
+
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "cpp", "rust", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = { enable = true, },
+}
+end
+  },
 }
