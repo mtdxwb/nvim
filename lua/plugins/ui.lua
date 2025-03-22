4186998d351f23bec85c424ad2637dbf3bdf1fab
mtdxwb <zdx1314520666@163.com>
2025-03-22 10:02:27 +0800

    添加nvim-cokeline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,4 +26,15 @@ return {
       vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
     end
   },
+
+  -- cokeline
+  {
+    "willothy/nvim-cokeline",
+    dependencies = {
+      "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
+      "nvim-tree/nvim-web-devicons", -- If you want devicons
+      "stevearc/resession.nvim"       -- Optional, for persistent history
+    },
+    config = true
+  },
 }
