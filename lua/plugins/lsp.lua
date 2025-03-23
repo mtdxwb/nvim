ffdd99aec4c441b7f0924280b6c5fdd28cc4808d
mtdxwb <zdx1314520666@163.com>
2025-03-23 10:22:44 +0800

    Update blink.cmp menu draw (border ==> 'single')

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,15 +63,21 @@ return {
         keyword = {
           range = 'full',     -- 在一个单词中间时，自动检测整个单词
         },
+
         list = {
           selection = { preselect = false, auto_insert = false },
         },
 
+        menu = { border = 'single' },
+
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 500,
+          window = { border = 'single' },
         },
       },
+
+      signature = { window = { border = 'single' } },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
