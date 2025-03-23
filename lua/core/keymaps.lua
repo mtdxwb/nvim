338c7a7c5c9f371cf6929692312ffdb00939b4c8
mtdxwb <zdx1314520666@163.com>
2025-03-23 12:15:56 +0800

    buffers之间切换[keymaps.lua]

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,33 +6,34 @@ local mode_v = { "v" }
 local mode_i = { "i" }
 
 -- telescope
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 local nmappings = {
-  -- 功能性设置
-  { from = ";",           to = ":" },
-  { from = "<leader>nh",  to = ":nohl<CR>" }, -- 取消高亮
+	-- 功能性设置
+	{ from = ";", to = ":" },
+	{ from = "<leader>nh", to = ":nohl<CR>" }, -- 取消高亮
 
-  -- 移动
-  { from = "<C-h>",       to = "<C-w>h" },
-  { from = "<C-j>",       to = "<C-w>j" },
-  { from = "<C-k>",       to = "<C-w>k" },
-  { from = "<C-l>",       to = "<C-w>l" },
-  { from = "J",           to = "5j",                    mode = mode_nv },
-  { from = "K",           to = "5k",                    mode = mode_nv },
+	-- 移动
+	{ from = "<C-h>", to = "<C-w>h" },
+	{ from = "<C-j>", to = "<C-w>j" },
+	{ from = "<C-k>", to = "<C-w>k" },
+	{ from = "<C-l>", to = "<C-w>l" },
+	{ from = "H", to = "<cmd>bprevious<CR>" },
+	{ from = "L", to = "<cmd>bnext<CR>" },
+	{ from = "J", to = "5j", mode = mode_nv },
+	{ from = "K", to = "5k", mode = mode_nv },
 
-  -- 移动单行或多行
-  { from = "mj",          to = ":m '>+1<CR>gv=gv",      mode = mode_v  },
-  { from = "mk",          to = ":m '<-2<CR>gv=gv",      mode = mode_v  },
+	-- 移动单行或多行
+	{ from = "mj", to = ":m '>+1<CR>gv=gv", mode = mode_v },
+	{ from = "mk", to = ":m '<-2<CR>gv=gv", mode = mode_v },
 
-  -- telescope
-  { from = "<leader>ff",  to = builtin.find_files,      opts = { desc = 'Telescope find files' } },
-  { from = "<leader>fg",  to = builtin.live_grep,       opts = { desc = 'Telescope live grep'  } },
-  { from = "<leader>fb",  to = builtin.buffers,         opts = { desc = 'Telescope buffers'    } },
-  { from = "<leader>fh",  to = builtin.help_tags,       opts = { desc = 'Telescope help tags'  } },
+	-- telescope
+	{ from = "<leader>ff", to = builtin.find_files, opts = { desc = "Telescope find files" } },
+	{ from = "<leader>fg", to = builtin.live_grep, opts = { desc = "Telescope live grep" } },
+	{ from = "<leader>fb", to = builtin.buffers, opts = { desc = "Telescope buffers" } },
+	{ from = "<leader>fh", to = builtin.help_tags, opts = { desc = "Telescope help tags" } },
 }
 
 for _, mapping in ipairs(nmappings) do
-  vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to,  mapping.opts or { noremap = true })
+	vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, mapping.opts or { noremap = true })
 end
-
