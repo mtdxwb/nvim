40d5cb83cf6a5cb7d8f2fdba4330d4e4792418c9
mtdxwb <zdx1314520666@163.com>
2025-04-04 07:56:23 +0800

    Add trans.nvim[tools.nvim]

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -160,4 +160,23 @@ return {
 
 	-- nvim-auto-pairs
 	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+
+	-- trans.nvim
+	{
+		"JuanZoran/Trans.nvim",
+		build = function()
+			require("Trans").install()
+		end,
+		keys = {
+			-- 可以换成其他你想映射的键
+			{ "<leader>tr", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = "󰊿 Translate" },
+			{ "<leader>tk", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
+			-- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
+			{ "<leader>ti", "<Cmd>TranslateInput<CR>", desc = "󰊿 Translate From Input" },
+		},
+		dependencies = { "kkharji/sqlite.lua" },
+		opts = {
+			-- your configuration there
+		},
+	},
 }
