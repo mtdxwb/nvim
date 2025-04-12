b612bb02e42dc391bc29f2f9b8d3f3087f99ae46
mtdxwb <zdx1314520666@163.com>
2025-04-12 14:49:29 +0800

    Add snacks.nvim[tools.lua]

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -2,13 +2,8 @@
 ---@type LazySpec
 return {
 	"mikavilpas/yazi.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"folke/snacks.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+  event = "VeryLazy",
+	dependencies = { "folke/snacks.nvim" },
 	keys = {
 		{ "<leader>e", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
 	},
