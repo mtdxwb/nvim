d69e18d516947cf10d208586ecc3f76d7d5cb578
mtdxwb <zdx1314520666@163.com>
2025-07-05 07:35:23 +0800

    code folding config

--- lua/core/defaults.lua
+++ lua/core/defaults.lua
@@ -28,3 +28,47 @@ opt.termguicolors = true
 opt.signcolumn = "yes"
 
 opt.updatetime = 300
+
+-- 代码折叠
+opt.foldenable = true
+opt.foldlevelstart = 99
+opt.foldmethod = 'expr'
+opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+opt.foldcolumn = '0'
+opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+local function fold_virt_text(result, start_text, lnum)
+  local text = ''
+  local hl
+  for i = 1, #start_text do
+    local char = start_text:sub(i, i)
+    local captured_highlights = vim.treesitter.get_captures_at_pos(0, lnum, i - 1)
+    local outmost_highlight = captured_highlights[#captured_highlights]
+    if outmost_highlight then
+      local new_hl = '@' .. outmost_highlight.capture
+      if new_hl ~= hl then
+        -- as soon as new hl appears, push substring with current hl to table
+        table.insert(result, { text, hl })
+        text = ''
+        hl = nil
+      end
+      text = text .. char
+      hl = new_hl
+    else
+      text = text .. char
+    end
+  end
+  table.insert(result, { text, hl })
+end
+function _G.custom_foldtext()
+  local start_text = vim.fn.getline(vim.v.foldstart):gsub('\t', string.rep(' ', vim.o.tabstop))
+  local nline = vim.v.foldend - vim.v.foldstart
+  local result = {}
+  fold_virt_text(result, start_text, vim.v.foldstart - 1)
+  table.insert(result, { ' ', nil })
+  table.insert(result, { '', '@comment.warning.gitcommit' })
+  table.insert(result, { '↙ ' .. nline .. ' lines', '@comment.warning' })
+  table.insert(result, { '', '@comment.warning.gitcommit' })
+  return result
+end
+vim.opt.foldtext = 'v:lua.custom_foldtext()'
