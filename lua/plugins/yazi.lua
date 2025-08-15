35cc80c3331b3ebefdbf693dcd12ac5821d7e63f
mtdxwb <zdx1314520666@163.com>
2025-08-15 09:53:13 +0800

    设置yazi全屏，命令Yazi cwd

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,14 +1,14 @@
 return {
 	"mikavilpas/yazi.nvim",
 	version = "*", -- use the latest stable version
-  lazy = false,
+	lazy = false,
 	dependencies = {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
 	keys = {
 		{
 			"<leader>-",
-			"<cmd>Yazi<CR>",
+			"<cmd>Yazi cwd<CR>",
 			desc = "Open yazi at the current file",
 			silent = true,
 		},
@@ -17,6 +17,8 @@ return {
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
 		open_for_directories = true,
+		floating_window_scaling_factor = 1.0,
+		yazi_floating_window_border = "none",
 		keymaps = {
 			show_help = "<f1>",
 		},
