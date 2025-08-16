8f5ab96a57ffa43fd4c13b6cfcfd9ba87b0838ca
mtdxwb <zdx1314520666@163.com>
2025-08-16 13:02:43 +0800

    设置fzf-lua不由keys触发

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,9 +3,10 @@ return {
 	-- optional for icon support
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
-	keys = {
-		{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "FzfLua find files" },
-		{ "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "FzfLua find buffers" },
-		{ "<leader>lg", "<cmd>FzfLua live_grep<CR>", desc = "FzfLua live grep" },
-	},
+  config = function(_, opts)
+    require("fzf-lua").setup(opts)
+    vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "FzfLua find files" })
+    vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", { desc = "FzfLua find buffers" })
+		vim.keymap.set("n", "<leader>lg", "<cmd>FzfLua live_grep<CR>", { desc = "FzfLua live grep" })
+  end,
 }
